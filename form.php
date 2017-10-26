1c9883929c7a7280f207bff81d3a6c5efcf5f398
Alex Moisi <amoisi@claritycon.com>
2017-10-26 15:51:34 -0700

    Add form, mobile menu, websitenotes page footer columns

---
+++ form.php
@@ -0,0 +1,13 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+	<meta charset="utf-8" />
+</head>
+<body>
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+<br>
+<a href ="index.html"> <button> Go back</button> </a>
+</body>
+</html>
